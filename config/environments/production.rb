ad55becd63e5609a7dd314fcd8e28859908c62ed
giancarlosio <giancarlos.isasi@gmail.com>
2016-08-28 00:15:56 -0500

    Change the config of databases

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'example.com',
+    domain: 'heroku.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['USER_GMAIL'],
